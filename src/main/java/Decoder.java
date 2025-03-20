cba078c0e6597d7e67520463aa68e903a9201114
Bohdan Metin <great.tuna.2020@gmail.com>
2025-03-20 22:56:03 +0200

    add getBencodedType method & BencodedStringType enum

--- src/main/java/Decoder.java
+++ src/main/java/Decoder.java
@@ -1,23 +1,46 @@
 public class Decoder {
+    private enum BencodedStringType {
+        STRING,
+        INTEGER,
+        LIST,
+        DICTIONARY,
+    }
+
     public String decodeBencode(String bencodedString) {
         if (bencodedString == null) {
             throw new NullPointerException("bencodedString must not be null");
         }
 
-        var firstCharacter = bencodedString.charAt(0);
-        if (firstCharacter >= '1' && firstCharacter <= '9') {
-            return this.decodeBencodedString(bencodedString);
-        }
-        return switch (firstCharacter) {
-            case 'i' -> "Integer";
-            case 'l' -> "Lists";
-            case 'd' -> "Dictionaries";
-            default -> throw new RuntimeException("Invalid string format");
+        BencodedStringType type = this.getBencodedType(bencodedString);
+        return switch (type) {
+            case BencodedStringType.STRING -> this.decodeBencodedString(bencodedString);
+            case BencodedStringType.INTEGER -> "INTEGER";
+            case BencodedStringType.LIST  -> "LIST";
+            case BencodedStringType.DICTIONARY -> "DICTIONARY";
         };
     }
 
     private String decodeBencodedString(String bencodedString) {
         int firstColonIndex = bencodedString.indexOf(':');
         return bencodedString.substring(firstColonIndex + 1);
+    }
+
+    private BencodedStringType getBencodedType(String bencodedString) {
+        var firstCharacter = bencodedString.charAt(0);
+        var lastCharacter = bencodedString.charAt(bencodedString.length() - 1);
+
+        if (firstCharacter >= '1' && firstCharacter <= '9') {
+            return BencodedStringType.STRING;
+        }
+        if (firstCharacter == 'i' && lastCharacter == 'e') {
+            return BencodedStringType.INTEGER;
+        }
+        if (firstCharacter == 'l' && lastCharacter == 'e') {
+            return BencodedStringType.LIST;
+        }
+        if (firstCharacter == 'd' && lastCharacter == 'e') {
+            return BencodedStringType.DICTIONARY;
+        }
+        throw new RuntimeException("Invalid string format");
     }
 }
